667508ad2d885295f7aa43256a88c7e817f29a08
mickfree <lydoived@gmail.com>
2024-11-19 22:17:00 -0500

    Add login & tables

--- manage_clients/views.py
+++ manage_clients/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from control.models import *
+from django.contrib.auth.views import LoginView
 
 
 # Create your views here.
 def index_manage_client(request):
-    client = Client.objects.first() # implements the function 'firts()' don't depend on a bucle
-    pet = Pet.objects.first()   
+    clients = Client.objects.all() # implements the function 'firts()' don't depend on a bucle
+    pets = Pet.objects.all()   
     context = {
-        'client':client,
-        'pet':pet
+        'clients':clients,
+        'pets':pets
     }
     return render(request, "manage_client_index.html", context) 
+
+
+class Login(LoginView):
+    template_name = 'login.html'
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        return response
